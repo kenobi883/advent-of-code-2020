20f5f73d590dd5b440fbcb37a78901f738079845
Marc Denning <marc.denning@gmail.com>
2020-12-01 22:27:24 -0500

    Day 1 input and solution code.

--- src/com/marcdenning/adventofcode/Day01.kt
+++ src/com/marcdenning/adventofcode/Day01.kt
@@ -1,5 +1,33 @@
 package com.marcdenning.adventofcode
 
-fun main(args: Array<String>) {
+import java.io.File
+import kotlin.system.exitProcess
 
+fun main(args: Array<String>) {
+    val expenseList = File(args[0]).readLines().map { it -> it.toInt() }
+    val numberOfOperands = args[1].toInt()
+
+    for ((index, expense) in expenseList.withIndex()) {
+        for (secondIndex in (index + 1) until expenseList.size) {
+            val secondExpense = expenseList[secondIndex]
+
+            if (numberOfOperands == 2 && 2020 == expense + secondExpense) {
+                println("$expense + $secondExpense = ${expense + secondExpense}")
+                println("$expense * $secondExpense = ${expense * secondExpense}")
+                exitProcess(0)
+            } else if (numberOfOperands == 3) {
+                for (thirdIndex in (secondIndex + 1) until expenseList.size) {
+                    val thirdExpense = expenseList[thirdIndex]
+
+                    if (2020 == expense + secondExpense + thirdExpense) {
+                        println("$expense + $secondExpense + $thirdExpense = ${expense + secondExpense + thirdExpense}")
+                        println("$expense * $secondExpense * $thirdExpense = ${expense * secondExpense * thirdExpense}")
+                        exitProcess(0)
+                    }
+                }
+            }
+        }
+    }
+    println("Could not find $numberOfOperands numbers that equal 2020")
+    exitProcess(1)
 }
