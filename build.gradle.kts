fd22310a5f5e14c53c06c5bcb2adccedffe0bef8
Marc Denning <marc.denning@gmail.com>
2020-12-06 12:06:40 -0500

    Add JUnit parameterized test dependency and configure Gradle Kotlin compiler.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 tasks.named<Test>("test") {
