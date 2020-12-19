458ec685cfe26da4d7b0e0e7a8d002c8de639077
Marc Denning <marc.denning@gmail.com>
2020-12-18 21:38:35 -0500

    Day 18 part 1 tests and solution.

--- src/main/kotlin/com/marcdenning/adventofcode/day18/Day18a.kt
+++ src/main/kotlin/com/marcdenning/adventofcode/day18/Day18a.kt
@@ -1,22 +1,40 @@
 package com.marcdenning.adventofcode.day18
 
-fun main(args: Array<String>) {
+import java.io.File
 
+fun main(args: Array<String>) {
+    val sum = File(args[0]).readLines().map { evaluateExpression(it) }.sum()
+
+    println("Sum of all operations: $sum")
 }
 
 fun evaluateExpression(expression: String): Long {
-    var total = 0L
-    var lastOperator = ' '
+    val expressionOperators = mutableListOf(' ')
+    val expressionTotals = mutableListOf(0L)
+    var expressionIndex = 0
 
     for (char in expression) {
         if (char.isDigit()) {
-            when (lastOperator) {
-                ' ', '+' -> total += ("" + char).toInt()
-                '*' -> total *= ("" + char).toInt()
+            when (expressionOperators[expressionIndex]) {
+                ' ', '+' -> expressionTotals[expressionIndex] = expressionTotals[expressionIndex] + ("" + char).toInt()
+                '*' -> expressionTotals[expressionIndex] = expressionTotals[expressionIndex] * ("" + char).toInt()
             }
         } else if (char == '*' || char == '+') {
-            lastOperator = char
+            expressionOperators[expressionIndex] = char
+        } else if (char == '(') {
+            expressionTotals.add(0L)
+            expressionOperators.add(' ')
+            expressionIndex++
+        } else if (char == ')') {
+            val expressionTotal = expressionTotals.removeAt(expressionIndex)
+
+            expressionOperators.removeAt(expressionIndex)
+            expressionIndex--
+            when (expressionOperators[expressionIndex]) {
+                ' ', '+' -> expressionTotals[expressionIndex] = expressionTotals[expressionIndex] + expressionTotal
+                '*' -> expressionTotals[expressionIndex] = expressionTotals[expressionIndex] * expressionTotal
+            }
         }
     }
-    return total
+    return expressionTotals[0]
 }
